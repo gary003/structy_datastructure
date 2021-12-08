5d5d44b7ac535c2635aa3573a5ef02b5c1aeeff7
gary003 <gary.johnson003@gmail.com>
2021-12-08 09:47:59 +0100

    hard ex... finished maxIncreasingSubsequence.js

--- 9.mixedRecall/maxIncreasingSubsequence.js
+++ 9.mixedRecall/maxIncreasingSubsequence.js
@@ -1,15 +1,31 @@
 const maxIncreasingSubsequence = (sequence) => {
-  const table = [[]]
+  if (sequence.length === 0) return 0
 
-  return table
+  const table = Array(sequence.length).fill(0)
+  table[0] = 1
+
+  for (let i = 1; i < sequence.length; i += 1) {
+    let max = 0
+    for (let j = 0; j < i; j += 1) {
+      if (sequence[j] < sequence[i] && table[j] > max) {
+        max = table[j]
+      }
+    }
+    table[i] = max + 1
+    // console.log(table)
+  }
+
+  return Math.max(...table)
 }
 
 const arrTest1 = []
 const arrTest2 = [9, 6, 4]
-const arrTest3 = [5, 2, 3, -4, 6, -1, 4, 9, 10]
-const arrTest4 = [15, 1, 6, 12, 5, 2, 1, 6, 3, 4, 9, 10, 12, 6, 12]
+const arrTest3 = [12, 9, 2, 5, 4, 32, 90, 20]
+const arrTest4 = [1, 4, 2, 3, 9, 5, 6, 7]
+const arrTest5 = [15, 1, 6, 12, 5, 2, 1, 6, 3, 4, 9, 10, 12, 6, 12]
 
 console.log(maxIncreasingSubsequence(arrTest1))
 console.log(maxIncreasingSubsequence(arrTest2))
 console.log(maxIncreasingSubsequence(arrTest3))
 console.log(maxIncreasingSubsequence(arrTest4))
+console.log(maxIncreasingSubsequence(arrTest5))
