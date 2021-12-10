acafde0073595d5354659f36d537c04ceccb809d
gary003 <gary.johnson003@gmail.com>
2021-12-10 10:46:53 +0100

    update subsets

--- 8.exhaustivRecursion/subsets.js
+++ 8.exhaustivRecursion/subsets.js
@@ -1,9 +1,17 @@
+/*
+subsets
+Write a function, subsets, that takes in an array as an argument.
+The function should return a 2D array where each subarray represents one of the possible subsets of the array.
+The elements within the subsets and the subsets themselves may be returned in any order.
+You may assume that the input array contains unique elements.
+*/
+
 const subsets = (elements, result = [[]]) => {
   if (elements.length === 0) return result
 
   const [element, ...rest] = elements
 
-  const newCombs = result.map((val) => [element, ...val])
+  const newCombs = result.map((val) => [...val, element])
   const oldCombs = [...result]
 
   result = [...oldCombs, ...newCombs]
@@ -13,4 +21,5 @@ const subsets = (elements, result = [[]]) => {
 
 console.log(subsets([])) // -> [[]]
 console.log(subsets(["x"])) // -> [[], ['x']]
-console.log(subsets(["a", "b"])) // -> [[], ['a'], ['b'], ['b', 'a']]
+console.log(subsets(["a", "b"])) // -> [[], ['a'], ['b'], ['a', 'b']]
+console.log(subsets(["a", "b", "c"]))
