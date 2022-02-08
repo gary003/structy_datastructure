45a7b52e84c9de4275916599b45e9cce4f6ceb8a
gary003 <gary.johnson003@gmail.com>
2022-02-08 23:56:10 +0100

    bfs and dfs cleanUp

--- 4.binaryTree/breathFirstValues.js
+++ 4.binaryTree/breathFirstValues.js
@@ -1,14 +1,5 @@
-// class Node {
-//   constructor(val) {
-//     this.val = val;
-//     this.left = null;
-//     this.right = null;
-//   }
-// }
-
 /*
 breadth first values
-
 Write a function, breadthFirstValues, that takes in the root of a binary tree. 
 The function should return an array containing all values of the tree in breadth-first order.
 */
@@ -31,3 +22,83 @@ const breadthFirstValues = (root) => {
 
   return arr
 }
+
+class Node {
+  constructor(val) {
+    this.val = val
+    this.left = null
+    this.right = null
+  }
+}
+
+const a1 = new Node("a")
+const b1 = new Node("b")
+const c1 = new Node("c")
+const d1 = new Node("d")
+const e1 = new Node("e")
+const f1 = new Node("f")
+a1.left = b1
+a1.right = c1
+b1.left = d1
+b1.right = e1
+c1.right = f1
+//      a
+//    /   \
+//   b     c
+//  / \     \
+// d   e     f
+console.log(breadthFirstValues(a1)) //    -> ['a', 'b', 'c', 'd', 'e', 'f']
+
+const a2 = new Node("a")
+const b2 = new Node("b")
+const c2 = new Node("c")
+const d2 = new Node("d")
+const e2 = new Node("e")
+const f2 = new Node("f")
+const g2 = new Node("g")
+const h2 = new Node("h")
+
+a2.left = b2
+a2.right = c2
+b2.left = d2
+b2.right = e2
+c2.right = f2
+e2.left = g2
+f2.right = h2
+//      a
+//    /   \
+//   b     c
+//  / \     \
+// d   e     f
+//    /       \
+//   g         h
+console.log(breadthFirstValues(a2)) // -> ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h']
+
+const a3 = new Node("a")
+//      a
+console.log(breadthFirstValues(a3)) // -> ['a']
+
+const a4 = new Node("a")
+const b4 = new Node("b")
+const c4 = new Node("c")
+const d4 = new Node("d")
+const e4 = new Node("e")
+const x4 = new Node("x")
+
+a4.right = b4
+b4.left = c4
+c4.left = x4
+c4.right = d4
+d4.right = e4
+
+//      a
+//       \
+//        b
+//       /
+//      c
+//    /  \
+//   x    d
+//         \
+//          e
+
+console.log(breadthFirstValues(a4)) // -> ['a', 'b', 'c', 'x', 'd', 'e']
--- 4.binaryTree/depthFirstValues.js
+++ 4.binaryTree/depthFirstValues.js
@@ -1,33 +1,115 @@
-// class Node {
-//   constructor(val) {
-//     this.val = val;
-//     this.left = null;
-//     this.right = null;
-//   }
-// }
-
 /*
 depth first values
-
 Write a function, depthFirstValues, that takes in the root of a binary tree. 
 The function should return an array containing all values of the tree in depth-first order.
 */
 
+// const depthFirstValues = (root) => {
+//   if (!root) return []
+
+//   const arr = []
+//   let stack = [root]
+
+//   while (stack.length > 0) {
+//     const currentValue = stack.pop()
+
+//     arr.push(currentValue.val)
+
+//     const nextValues = [currentValue.right, currentValue.left].filter((val) => !!val)
+
+//     stack = [...stack, ...nextValues]
+//   }
+
+//   return arr
+// }
+
 const depthFirstValues = (root) => {
-  if (!root) return []
+  if (root === null) return []
 
-  const arr = []
-  let stack = [root]
+  const res = [root.val]
 
-  while (stack.length > 0) {
-    const currentValue = stack.pop()
-
-    arr.push(currentValue.val)
-
-    const nextValues = [currentValue.right, currentValue.left].filter((val) => !!val)
-
-    stack = [...stack, ...nextValues]
+  if (root.left !== null) {
+    res.push(...depthFirstValues(root.left))
   }
 
-  return arr
+  if (root.right !== null) {
+    res.push(...depthFirstValues(root.right))
+  }
+
+  return res
 }
+
+class Node {
+  constructor(val) {
+    this.val = val
+    this.left = null
+    this.right = null
+  }
+}
+
+const a0 = new Node("a")
+const b0 = new Node("b")
+const c0 = new Node("c")
+const d0 = new Node("d")
+const e0 = new Node("e")
+const f0 = new Node("f")
+a0.left = b0
+a0.right = c0
+b0.left = d0
+b0.right = e0
+c0.right = f0
+//      a
+//    /   \
+//   b     c
+//  / \     \
+// d   e     f
+console.log(depthFirstValues(a0)) // -> ['a', 'b', 'd', 'e', 'c', 'f']
+
+const a1 = new Node("a")
+const b1 = new Node("b")
+const c1 = new Node("c")
+const d1 = new Node("d")
+const e1 = new Node("e")
+const f1 = new Node("f")
+const g1 = new Node("g")
+a1.left = b1
+a1.right = c1
+b1.left = d1
+b1.right = e1
+c1.right = f1
+e1.left = g1
+//      a
+//    /   \
+//   b     c
+//  / \     \
+// d   e     f
+//    /
+//   g
+console.log(depthFirstValues(a1)) //  -> ['a', 'b', 'd', 'e', 'g', 'c', 'f']
+
+const a2 = new Node("a")
+console.log(depthFirstValues(a2)) // -> ['a']
+
+const a3 = new Node("a")
+const b3 = new Node("b")
+const c3 = new Node("c")
+const d3 = new Node("d")
+const e3 = new Node("e")
+
+a3.right = b3
+b3.left = c3
+c3.right = d3
+d3.right = e3
+
+//      a
+//       \
+//        b
+//       /
+//      c
+//       \
+//        d
+//         \
+//          e
+console.log(depthFirstValues(a3)) // -> ['a', 'b', 'c', 'd', 'e']
+
+console.log(depthFirstValues(null)) // -> []
